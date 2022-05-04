8016b236217b7aa434f7329ec05717ba633da342
asaikovski <asaikovski@microsoft.com>
2022-05-04 12:07:16 +1000

    added new packages

--- chocolately-install.ps1
+++ chocolately-install.ps1
@@ -9,15 +9,20 @@ choco install sublimetext3 -y
 choco install 7zip -y
 choco install notepadplusplus -y
 choco install cmder -y
-choco install fiddler4 -y
+#choco install fiddler4 -y
 choco install paint.net -y
 choco install kdiff3 -y
 choco install git -y
 choco install gitextensions -y
-choco install putty -y
-choco install armclient -y
-choco install gimp -y
-choco install mysql.workbench -y
-choco install webpi -y
+#choco install putty -y
+#choco install armclient -y
+#choco install gimp -y
+#choco install mysql.workbench -y
+#choco install webpi -y
+choco install vscode
+choco install azure-cli
+choco install bicep
+choco install slack
+choco install adobereader
 
 cup all -y
